3d8d00dd7f314dfa89d3dd7886b0cd3de891bf4a
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-13 04:56:13 -0400

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# This code write by Mr.nope
+clear
+echo "Uninstalling..."
+cd /usr/lib/python3.6 && sudo rm run.pyc
+sleep 2
+echo ""
+echo "Uninstalling..., Finish...!"
+echo ""
+exit 1
