2eee2307d6060c19d38826c6065a0744cce80b94
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-13 05:18:04 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This code write by Mr.nope
+clear
+sudo apt install python
+sudo apt install python3
+echo "Installing..."
+sleep 2
+sudo cp run.pyc /usr/lib/python3.6
+echo ""
+echo "Installing..., Finish...!"
+echo ""
+exit 1
